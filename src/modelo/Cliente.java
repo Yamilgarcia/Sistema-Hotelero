205507dbdac0f6e84f8893b7efbda37fa4d603c7
Yamil <yamilg620@gmail.com>
2023-05-16 17:57:58 -0600

    Metodos get y constructores realizados

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -9,5 +9,89 @@ package modelo;
  * @author Usuario
  */
 public class Cliente {
+    int ID_cliente;
+    String Cedula_cliente;
+    String Nombre1;
+    String Nombre2;
+    String Apellido1;
+    String Apellido2;
+    String Procedencia;
+    int Telefono;
+
+    public Cliente(int ID_cliente, String Cedula_cliente, String Nombre1, String Nombre2, String Apellido1, String Apellido2, String Procedencia, int Telefono) {
+        this.ID_cliente = ID_cliente;
+        this.Cedula_cliente = Cedula_cliente;
+        this.Nombre1 = Nombre1;
+        this.Nombre2 = Nombre2;
+        this.Apellido1 = Apellido1;
+        this.Apellido2 = Apellido2;
+        this.Procedencia = Procedencia;
+        this.Telefono = Telefono;
+    }
+
+    public int getID_cliente() {
+        return ID_cliente;
+    }
+
+    public void setID_cliente(int ID_cliente) {
+        this.ID_cliente = ID_cliente;
+    }
+
+    public String getCedula_cliente() {
+        return Cedula_cliente;
+    }
+
+    public void setCedula_cliente(String Cedula_cliente) {
+        this.Cedula_cliente = Cedula_cliente;
+    }
+
+    public String getNombre1() {
+        return Nombre1;
+    }
+
+    public void setNombre1(String Nombre1) {
+        this.Nombre1 = Nombre1;
+    }
+
+    public String getNombre2() {
+        return Nombre2;
+    }
+
+    public void setNombre2(String Nombre2) {
+        this.Nombre2 = Nombre2;
+    }
+
+    public String getApellido1() {
+        return Apellido1;
+    }
+
+    public void setApellido1(String Apellido1) {
+        this.Apellido1 = Apellido1;
+    }
+
+    public String getApellido2() {
+        return Apellido2;
+    }
+
+    public void setApellido2(String Apellido2) {
+        this.Apellido2 = Apellido2;
+    }
+
+    public String getProcedencia() {
+        return Procedencia;
+    }
+
+    public void setProcedencia(String Procedencia) {
+        this.Procedencia = Procedencia;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(int Telefono) {
+        this.Telefono = Telefono;
+    }
+    
     
 }
--- src/modelo/Reservacion.java
+++ src/modelo/Reservacion.java
@@ -3,11 +3,65 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
-
+import java.sql.Date;
 /**
  *
  * @author Usuario
  */
 public class Reservacion {
+    int ID_Reservaciones;
+    int ID_cliente;
+    Date F_entreda;
+    Date F_salida;
+    int ID_Empleado;
+
+    public Reservacion(int ID_Reservaciones, int ID_cliente, Date F_entreda, Date F_salida, int ID_Empleado) {
+        this.ID_Reservaciones = ID_Reservaciones;
+        this.ID_cliente = ID_cliente;
+        this.F_entreda = F_entreda;
+        this.F_salida = F_salida;
+        this.ID_Empleado = ID_Empleado;
+    }
+
+    public int getID_Reservaciones() {
+        return ID_Reservaciones;
+    }
+
+    public void setID_Reservaciones(int ID_Reservaciones) {
+        this.ID_Reservaciones = ID_Reservaciones;
+    }
+
+    public int getID_cliente() {
+        return ID_cliente;
+    }
+
+    public void setID_cliente(int ID_cliente) {
+        this.ID_cliente = ID_cliente;
+    }
+
+    public Date getF_entreda() {
+        return F_entreda;
+    }
+
+    public void setF_entreda(Date F_entreda) {
+        this.F_entreda = F_entreda;
+    }
+
+    public Date getF_salida() {
+        return F_salida;
+    }
+
+    public void setF_salida(Date F_salida) {
+        this.F_salida = F_salida;
+    }
+
+    public int getID_Empleado() {
+        return ID_Empleado;
+    }
+
+    public void setID_Empleado(int ID_Empleado) {
+        this.ID_Empleado = ID_Empleado;
+    }
+    
     
 }
